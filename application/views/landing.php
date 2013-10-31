e45eb3b362144f43f12e2d230b356e37605a6249
Kester Arenas <klfarenas@gmail.com>
2013-10-31 17:50:22 +0800

    modified carousel, Contact Page, etc

--- application/views/landing.php
+++ application/views/landing.php
@@ -45,153 +45,35 @@
 		</div>
 		
 		<div class = 'container'> 
-			
-			<div class = 'row padded'>
-				<div class = 'col-md-2 start-block maroon'>
-					<h1>News</h1>
-					<a href = ''>More News &rarr;</a>
+			<div class = 'row'>
+				<div class="col-md-6 well">
+					<div class="panel panel-default">
+					  <div class="panel-heading">
+					    <h2 class="panel-title">News</h2>
+					  </div>
+					  <div class="panel-body list-group">
+						<a href="#" class="list-group-item">Attention Graduating Students, the Enrollment...</a>
+						<a href="#" class="list-group-item">No Classes on Monday because of ongoing Enrollment</a>
+						<a href="#" class="list-group-item">CS165 under Sir Edgar Felizmenio will be dissolved...</a>
+						</ul>
+					  </div>
+					</div>
 				</div>
-				<div class = 'col-md-10 end-block'>
-					<div class = 'full-height'>
-						<div class = 'viewport'> 
-							<div class = 'wrapper'>
-								<div>
-									<h6>Some news has been added! 1<h6>
-									<p class = 'details'>
-									As of now, a handful of news articles had been added...
-									</p class = 'details'>
-									<p>
-									<a href = ''>Read more &rarr;</a>
-									</p>
-								</div>
-								<div>
-									<h6>Some news has been added! 2<h6>
-									<p class = 'details'>
-									As of now, a handful of news articles had been added...
-									</p>
-									<p>
-									<a href = ''>Read more &rarr;</a>
-									</p>
-								</div>
-								<div>
-									<h6>Some news has been added! 3<h6>
-									<p class = 'details'>
-									As of now, a handful of news articles had been added...
-									</p>
-									<p>
-									<a href = ''>Read more &rarr;</a>
-									</p>
-								</div>
-								<div>
-									<h6>Some news has been added! 4<h6>
-									<p class = 'details'>
-									As of now, a handful of news articles had been added...
-									</p>
-									<p>
-									<a href = ''>Read more &rarr;</a>
-									</p>
-								</div>
-								<div>
-									<h6>Some news has been added! 5<h6>
-									<p class = 'details'>
-									As of now, a handful of news articles had been added...
-									</p>
-									<p>
-									<a href = ''>Read more &rarr;</a>
-									</p>
-								</div>
-								<div>
-									<h6>Some news has been added! 6<h6>
-									<p class = 'details'>
-									As of now, a handful of news articles had been added...
-									</p>
-									<p>
-									<a href = ''>Read more &rarr;</a>
-									</p>
-								</div>
-								<div>
-									<h6>Some news has been added! 7<h6>
-									<p class = 'details'>
-									As of now, a handful of news articles had been added...
-									</p>
-									<p>
-									<a href = ''>Read more &rarr;</a>
-									</p>
-								</div>
-								<div>
-									<h6>Some news has been added! 8<h6>
-									<p class = 'details'>
-									As of now, a handful of news articles had been added...
-									</p>
-									<p>
-									<a href = ''>Read more &rarr;</a>
-									</p>
-								</div>
-							</div> 
-						</div>
-						<div class = 'arrows'>
-							<div>
-								&larr;
-							</div>
-							<div>
-								&rarr;
-							</div>
-						</div>
+				
+				<div class="col-md-6 well">
+					<div class="panel panel-danger">
+					  <div class="panel-heading">
+					    <h2 class="panel-title">Announcements</h2>
+					  </div>
+					  <div class="panel-body list-group">
+						<a href="#" class="list-group-item">Attention Graduating Students, the Enrollment...</a>
+						<a href="#" class="list-group-item">No Classes on Monday because of ongoing Enrollment</a>
+						<a href="#" class="list-group-item">CS165 under Sir Edgar Felizmenio will be dissolved...</a>
+						</ul>
+					  </div>
 					</div>
 				</div>
 			</div>
-			<div class = 'row padded'>
-				<div class = 'col-md-2 start-block orange'>
-					<h1>Events</h1>
-					<a href = ''>More Events &rarr;</a>
-				</div>
-				<div class = 'col-md-10 end-block'>
-					<div class = 'full-height'>
-						<div class = 'viewport'> 
-							<div class = 'wrapper'>
-								<div>
-									<h6>13 April 2013<h6>
-									<p>
-									World Something Day
-									</p> 
-								</div>
-								<div>
-									<h6>14 April 2013<h6>
-									<p>
-									Tope's Day
-									</p> 
-								</div>
-								<div>
-									<h6>15 April 2013<h6>
-									<p>
-									Some Day
-									</p> 
-								</div> 
-								<div>
-									<h6>20 April 2013<h6>
-									<p>
-									Grad Detail 2
-									</p> 
-								</div> 
-								<div>
-									<h6>28 April 2013<h6>
-									<p>
-									102th Commencement Exercises
-									</p> 
-								</div> 
-							</div> 
-						</div>
-						<div class = 'arrows'>
-							<div>
-								&larr;
-							</div>
-							<div>
-								&rarr;
-							</div>
-						</div>
-					</div>
-				</div>
-			</div> 
 		</div>
 <!-- Load Footer-->
 <?php $this->load->view('includes/footer', array('tag' =>'landing')); ?>
