df3687d8b898cce36b98819b9c302ed6a009111e
Jym Paul A. Carandang <pauljym_carandang@yahoo.com>
2013-11-26 21:32:20 +0800

    Edited landing page

--- application/views/landing.php
+++ application/views/landing.php
@@ -5,7 +5,7 @@
 		<!-- Load Navigation Bar-->
 		<?php $this->load->view('includes/nav'); ?>
 		
-		<div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+		<div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="5000">
 		  <!-- Indicators -->
 		  <!--<ol class="carousel-indicators">
 		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
@@ -16,19 +16,19 @@
 		  <!-- Wrapper for slides -->
 		  <div class="carousel-inner">
 		    <div class="item active">
-		      <img src="holder.js/1200x400/gray" >
+		      <img src="holder.js/1280x400/gray" class='img-responsive'>
 		      <div class="carousel-caption">
 		      <p style="color: black;">item 1<p>
 		      </div>
 		    </div>
 		    <div class="item">
-		      <img src="holder.js/1200x400/industrial" >
+		      <img src="holder.js/1280x400/industrial" class='img-responsive'>
 		      <div class="carousel-caption">
 		      <p style="color: black;">item 2<p>
 		      </div>
 		    </div>
 		    <div class="item">
-		      <img src="holder.js/1200x400/social" >
+		      <img src="holder.js/1280x400/social" class='img-responsive'>
 		      <div class="carousel-caption">
 		      <p style="color: black;">item 3<p>
 		      </div>
@@ -44,22 +44,22 @@
 		  </a>
 		</div>
 		
-		<div class = 'container'> 
+		<div class = 'container landing'>
 			<div class = 'row'>
 				<div class="col-md-6">
 					<div class="panel panel-primary">
 					  <div class="panel-heading">
-					    <h1 class="panel-title">News</h1>
+					    <h3 class="panel-title">News</h3>
 					  </div>
 					<div class="panel-body">
 						<div class='panel panel-info'>
-							<div class='panel-heading'><a href='#'><h2 class='panel-title'>Lorem Ipsum</h3></a></div>
+							<div class='panel-heading'><a href='#'><h1 class='panel-title'>Lorem Ipsum</h1></a></div>
 							<div class='panel-body'>
 							<p>
-							<img src='http://placehold.it/128x128' class='img-thumbnail img-responsive pull-right'>
+							<img src='holder.js/128x128/social' class='img-thumbnail img-responsive pull-right'>
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique ultrices purus, nec malesuada mauris laoreet placerat. Mauris ultricies eu felis id auctor. Integer quis convallis turpis. Vivamus eget est id neque hendrerit suscipit vitae sit amet tortor. Fusce eu dignissim mauris, id fringilla lacus. Proin posuere accumsan lorem, quis tincidunt orci dignissim a. Nullam viverra ligula eget magna sodales faucibus. Praesent vel ultrices turpis. Cras vitae congue purus. Sed et laoreet lorem, non scelerisque sapien.
 							</p><hr/>
-							<a href='#' class='pull-right'>Read More &rarr;</a>
+							<a href='#' class='pull-right' style="font-size:12px;">Read More &rarr;</a>
 							</div>
 
 							<ul class='list-group'>
@@ -67,6 +67,7 @@
 								<a href="#" class="list-group-item"><strong>CS165 under Sir Edgar Felizmenio will be dissolved...</strong></a>
 							</ul>
 						</div>
+						<hr/>
 						<a href='#' class='pull-right'><strong>See All News &rarr;</strong></a>
 					  </div>
 					</div>
@@ -78,18 +79,22 @@
 					    <h1 class="panel-title">Announcements</h1>
 					  </div>
 					  <div class="panel-body">
-						<div class='alert alert-danger'><a href='#' class='alert-link'>Attention Graduating Students, the Enrollment...</a></div><hr/>
-						<div class='alert alert-danger'><a href='#' class='alert-link'>No Classes on Monday because of ongoing Enrollment</a></div><hr/>
-						<div class='alert alert-danger'><a href='#' class='alert-link'>CS165 under Sir Edgar Felizmenio will be dissolved...</a></div>
-						
-						<span class='pull-right'><a href='#'>See All Announcements&rarr;</a></span>
+						<ul class='list-group'>
+							<strong>
+							<a href='#' class='list-group-item'>Attention Graduating Students, the Enrollment...</a>
+							<a href='#' class='list-group-item'>No Classes on Monday because of ongoing Enrollment</a>
+							<a href='#' class='list-group-item'>CS165 under Sir Edgar Felizmenio will be dissolved...</a>
+							</strong>
+						</ul>
+						<hr/>
+						<a href='#' class='pull-right'><strong>See All Announcements&rarr;</strong></a>
 					  </div>
 					</div>
 				</div>
 				
 			</div>
 				
-			<div class='panel panel-info'>
+			<div class='panel panel-default'>
 				<div class='panel-heading'>
 					<h1 class='panel-title'>Events</h1>
 				</div>
@@ -108,11 +113,11 @@
 							<p>New Year.</p>
 						</div>
 					</div>
-					<span class='pull-right'><a href='#'>See All Events&rarr;</a></span>
+					<hr/>
+					<a href='#' class='pull-right'><strong>See All Events&rarr;</strong></a>
 				</div>
 			</div> <!-- end of panel -->
 					
-			
 		</div>
 <!-- Load Footer-->
 <?php $this->load->view('includes/footer', array('tag' =>'landing')); ?>
