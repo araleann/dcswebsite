6996a16df245f9d1b3864255ac25cbfb8421c162
Kester Arenas <klfarenas@gmail.com>
2013-11-30 19:48:26 +0800

    created Event Calendar page template

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -32,7 +32,82 @@ class Main extends CI_Controller {
 		$this->load->view('test');
 	}
 
-}
+	public function calendar(){
 
+		/*$prefs['template'] = '
+			{table_open}<table border="0" cellpadding="0" cellspacing="0" class="calendar">{/table_open}
+
+			{heading_row_start}<tr>{/heading_row_start}
+
+			{heading_previous_cell}<th class="center"><a href="{previous_url}"><i class="fa fa-angle-left"></i></a></th>{/heading_previous_cell}
+			{heading_title_cell}<th class="center" colspan="{colspan}">{heading}</th>{/heading_title_cell}
+			{heading_next_cell}<th class="center"><a class="center" colspan="2" href="{next_url}"><i class="fa fa-angle-right"></i></a></th>{/heading_next_cell}
+
+			{heading_row_end}</tr>{/heading_row_end}
+
+			{week_row_start}<tr>{/week_row_start}
+			{week_day_cell}<th class="day_header">{week_day}</th>{/week_day_cell}
+			{week_row_end}</tr>{/week_row_end}
+
+			{cal_row_start}<tr>{/cal_row_start}
+			{cal_cell_start}<td>{/cal_cell_start}
+
+			{cal_cell_content}<div class="hoverable day_listing" href="{content}">{day}</div>{/cal_cell_content}
+		    {cal_cell_content_today}<div class="today day_listing" href="{content}">{day}</div>{/cal_cell_content_today}
+
+			{cal_cell_no_content}<div class="hoverable day_listing">{day}</div>{/cal_cell_no_content}
+		    {cal_cell_no_content_today}<div class="today day_listing">{day}</div>{/cal_cell_no_content_today}
+
+			{cal_cell_blank}&nbsp;{/cal_cell_blank}
+
+			{cal_cell_end}</td>{/cal_cell_end}
+			{cal_row_end}</tr>{/cal_row_end}
+
+			{table_close}</table>{/table_close}
+		';*/
+
+		$prefs['template'] = '
+
+			{table_open}<table border="0" cellpadding="0" cellspacing="0" class="calendar">{/table_open}
+
+			{heading_row_start}<tr class="calendar-title">{/heading_row_start}
+
+			{heading_previous_cell}<th></th><th class="center calendar-arrow"><a href="{previous_url}"><i class="fa fa-angle-left fa-2x" style="color:#838282"></i></a></th>{/heading_previous_cell}
+			{heading_title_cell}<th class="center" colspan="3"><h4>{heading}</h4></th>{/heading_title_cell}
+			{heading_next_cell}<th class="center calendar-arrow"><a href="{next_url}"><i class="fa fa-angle-right fa-2x" style="color:#838282"></i></a></th><th></th>{/heading_next_cell}
+
+			{heading_row_end}</tr>{/heading_row_end}
+
+			{week_row_start}<tr>{/week_row_start}
+			{week_day_cell}<td class="day_header">{week_day}</td>{/week_day_cell}
+			{week_row_end}</tr>{/week_row_end}
+
+			{cal_row_start}<tr>{/cal_row_start}
+			{cal_cell_start}<td class="calendar-day">{/cal_cell_start}
+
+			{cal_cell_content}<a href="{content}" data-toggle="modal" data-target="#myModal">{day}</a>{/cal_cell_content}
+			{cal_cell_content_today}<div class="today"><a href="{content}" data-toggle="modal" data-target="#myModal">{day}</a></div>{/cal_cell_content_today}
+
+			{cal_cell_no_content}{day}{/cal_cell_no_content}
+			{cal_cell_no_content_today}<div class="today">{day}</div>{/cal_cell_no_content_today}
+
+			{cal_cell_blank}&nbsp;{/cal_cell_blank}
+
+			{cal_cell_end}</td>{/cal_cell_end}
+			{cal_row_end}</tr>{/cal_row_end}
+
+			{table_close}</table>{/table_close}
+		';
+
+		$prefs['show_next_prev'] = TRUE;
+		$prefs['next_prev_url'] = 'http://localhost/myfiles/new_dcsw/index.php/main/calendar';
+
+		$this->load->library('calendar', $prefs);
+
+		//echo $this->calendar->generate($this->uri->segment(3), $this->uri->segment(4));
+		
+		$this->load->view('calendar');
+	}
+}
 /* End of file main.php */
 /* Location: ./application/controllers/main.php */
---
+++ application/views/calendar.php
@@ -0,0 +1,46 @@
+<!-- Load Header -->
+<?php $this->load->view('includes/header'); ?>
+
+<body>		
+		
+		<!-- Load Navigation Bar-->
+		<?php $this->load->view('includes/nav'); ?>
+
+	<div class="container">
+		<div class="col-md-offset-2 col-md-8 content">
+			<?php
+				$data = array(
+					3  => 'http://example.com/news/article/2006/03/',
+					7  => 'http://example.com/news/article/2006/07/',
+					13 => 'http://example.com/news/article/2006/13/',
+					26 => 'http://example.com/news/article/2006/26/'
+				);
+
+				echo $this->calendar->generate($this->uri->segment(3), $this->uri->segment(4), $data);
+			?>
+		</div>
+	</div>
+	<div class="container ">
+		<div class="col-md-offset-2 col-md-8"><h3>Jump to</h3></div>
+	</div>
+
+	<div class="modal" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Insert Selected Date Here</h4>
+      </div>
+      <div class="modal-body">
+        <ul>
+        <li>Event 1</li>
+		<li>Event 2</li>
+		<li>Event 3</li>
+		</ul>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!-- Load Footer-->
+<?php $this->load->view('includes/footer', array('tag' => 'landing')); ?>
